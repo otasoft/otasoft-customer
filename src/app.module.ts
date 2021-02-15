97139b0a22fb410d5032190389052b845af4d1ef
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-15 17:00:04 +0100

    refactor-#29: use typeormconfigservice for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { DbModule } from './db/db.module';
+import { DatabaseModule } from './database/database.module';
 import { CustomerModule } from './customer/customer.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DbModule, CustomerModule, HealthModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CustomerModule, HealthModule],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TypeOrmConfigService } from './config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
+})
+export class DatabaseModule {}
